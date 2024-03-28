8d8bcff256a10c60650a4df46266ad8745f3f82c
Dionysists <d.tsouchlakis@auroraer.com>
2024-03-28 22:34:18 +0900

    Updates

--- _next/static/KC9cI0GMCCOCFFl--JD3s/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/385-1d35d79b7f11dc91.js","static/css/3cbd70ff48da2307.css","static/chunks/pages/index-246800f6f7c7407e.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
